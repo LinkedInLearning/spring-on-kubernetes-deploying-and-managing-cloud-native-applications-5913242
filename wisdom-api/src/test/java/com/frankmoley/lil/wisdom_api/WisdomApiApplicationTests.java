460b8fe60d00d06f9490220cc666cfe833ad82e2
Frank P Moley III <frankmoley@gmail.com>
2024-08-04 03:10:45 +0000

    adding 01_02 files

---
+++ wisdom-api/src/test/java/com/frankmoley/lil/wisdom_api/WisdomApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frankmoley.lil.wisdom_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WisdomApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
