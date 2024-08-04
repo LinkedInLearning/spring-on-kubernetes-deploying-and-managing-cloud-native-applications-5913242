fb8f078d940477ca4e357a1322d81bef528ea7dc
Frank P Moley III <frankmoley@gmail.com>
2024-08-04 03:10:45 +0000

    adding 01_02 files

---
+++ wisdom-api/src/main/java/com/frankmoley/lil/wisdom/WisdomApiApplication.java
@@ -0,0 +1,13 @@
+package com.frankmoley.lil.wisdom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WisdomApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WisdomApiApplication.class, args);
+	}
+
+}
