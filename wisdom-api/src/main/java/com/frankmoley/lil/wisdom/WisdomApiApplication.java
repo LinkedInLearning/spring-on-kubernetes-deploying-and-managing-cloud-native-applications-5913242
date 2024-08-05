14ce1303a024fabc3c2f2117c5c72eef85701cf0
Frank P Moley III <frankmoley@gmail.com>
2024-08-05 02:32:02 +0000

    files for 01_09

--- wisdom-api/src/main/java/com/frankmoley/lil/wisdom/WisdomApiApplication.java
+++ wisdom-api/src/main/java/com/frankmoley/lil/wisdom/WisdomApiApplication.java
@@ -1,13 +1,23 @@
 package com.frankmoley.lil.wisdom;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.micrometer.core.instrument.MeterRegistry;
 
 @SpringBootApplication
 public class WisdomApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WisdomApiApplication.class, args);
+	}
+
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> metricsCommonTags(@Value("${spring.application.name}")String appName){
+		return registry -> registry.config().commonTags("app", appName);
 	}
 
 }
--- wisdom-api/src/main/java/com/frankmoley/lil/wisdom/web/CustomerController.java
+++ wisdom-api/src/main/java/com/frankmoley/lil/wisdom/web/CustomerController.java
@@ -1,7 +1,9 @@
 package com.frankmoley.lil.wisdom.web;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -22,6 +24,8 @@ import com.frankmoley.lil.wisdom.data.repository.CustomerRepository;
 import com.frankmoley.lil.wisdom.util.exception.BadRequestException;
 import com.frankmoley.lil.wisdom.util.exception.NotFoundException;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PutMapping;
 
@@ -32,23 +36,40 @@ import org.springframework.web.bind.annotation.PutMapping;
 public class CustomerController {
 
   private final CustomerRepository customerRepository;
+  private final Map<String, Timer> timerMap;
 
-  public CustomerController(CustomerRepository customerRepository){
+  private static final String GET_ALL_CUSTOMERS = "getAllCustomers";
+  private static final String ADD_CUSTOMER = "addCustomer";
+  private static final String GET_CUSTOMER = "getCustomer";
+  private static final String UPDATE_CUSTOMER = "updateCustomer";
+  private static final String DELETE_CUSTOMER = "deleteCustomer";
+
+  public CustomerController(CustomerRepository customerRepository, MeterRegistry registry){
     super();
     this.customerRepository = customerRepository;
+    timerMap = new HashMap<>();
+    timerMap.put(GET_ALL_CUSTOMERS, registry.timer(GET_ALL_CUSTOMERS));
+    timerMap.put(ADD_CUSTOMER, registry.timer(ADD_CUSTOMER));
+    timerMap.put(GET_CUSTOMER, registry.timer(GET_CUSTOMER));   
+    timerMap.put(UPDATE_CUSTOMER, registry.timer(UPDATE_CUSTOMER));
+    timerMap.put(DELETE_CUSTOMER, registry.timer(DELETE_CUSTOMER));
   }
 
   @GetMapping
   public Iterable<Customer> getAllCustomers(@RequestParam(required=false)String email){
+    Timer.Sample timer = Timer.start();
     if(StringUtils.hasLength(email)){
       List<Customer> customers = new ArrayList<>();
       Optional<Customer> customer = this.customerRepository.findByEmail(email);
       if(customer.isPresent()){
         customers.add(customer.get());
       }
+      timerMap.get(GET_ALL_CUSTOMERS).record(()->timer.stop(timerMap.get(GET_ALL_CUSTOMERS)));
       return customers;
     }
-    return this.customerRepository.findAll();
+    Iterable<Customer> customers = this.customerRepository.findAll();
+    timerMap.get(GET_ALL_CUSTOMERS).record(() -> timer.stop(timerMap.get(GET_ALL_CUSTOMERS)));
+    return customers;
   }
 
   @PostMapping
